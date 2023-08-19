3ce6eab387e14458e61195fa503636298dada0bd
kevx <kevx@umich.edu>
2023-08-19 14:12:51 +0800

    start landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
-  return (
-    <main className="main">
-        <h1>LinkReads</h1>
-    </main>
-  )
+    return (
+        <main className="main min-h-screen px-32">
+            <nav id="navbar" className="p-2 mt-2">
+                <h1 className="font-serif font-bold text-5xl">LinkReads</h1>
+            </nav>
+            <div id="homepage" className="">
+                <div id="hero-container" className="">
+                    <div className="">
+                        <h1 className="font-sans font-bold text-xl">A platform for digital readers</h1>
+                        <h2 className="font-sans font-medium text-lg">Track what you read online, and see what your friends are reading</h2>
+                        <button>Sign up</button>
+                        <button>Log in</button>
+                    </div>
+                </div>
+           from </div>
+        </main>
+    )
 }
