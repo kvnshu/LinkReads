03d5712575b0d2949a731d9cc196bdce4b48e0f2
kevx <kevx@umich.edu>
2023-08-20 23:39:50 +0800

    finish homepage static stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
+import Navbar from './nav'
 export default function Home() {
     return (
-        <main className="main min-h-screen px-32">
-            <nav id="navbar" className="p-2 mt-2">
-                <h1 className="font-serif font-bold text-5xl">LinkReads</h1>
-            </nav>
-            <div id="homepage" className="">
+        <div className="main ">
+            <Navbar />
+            <div id="homepage" className="min-h-screen px-32">
                 <div id="hero-container" className="">
                     <div className="">
                         <h1 className="font-sans font-bold text-xl">A platform for digital readers</h1>
                         <h2 className="font-sans font-medium text-lg">Track what you read online, and see what your friends are reading</h2>
-                        <button>Sign up</button>
-                        <button>Log in</button>
                     </div>
                 </div>
-           from </div>
-        </main>
+            </div>
+        </div>
     )
 }
