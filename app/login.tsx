633200f932335ed5393aa0455b6bf9b82c1468f2
kevx <kevx@umich.edu>
2023-08-21 15:05:55 +0800

    implement google sign in with auth-next

---
+++ app/login.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useSession, signIn, signOut } from "next-auth/react"
+import React from "react"
+
+export default function Component() {
+    const { data: session } = useSession()
+    if (session) {
+        return (
+            <>
+                {/* Signed in as {session.user.email} <br /> */}
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        )
+    }
+    return (
+        <>
+            <button onClick={() => signIn("google")}>Sign in with Google</button>
+        </>
+    )
+}
