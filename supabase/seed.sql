d62b06bf12695d08561d5764b18e184049d1f284
Kevin Xu <k3vn.xu@gmail.com>
2023-10-21 11:50:38 -0400

    comment out supabase commands

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,5 @@
-insert into todos(title)
-values
-  ('Create Supabase project'),
-  ('Create Next.js app from Supabase Starter template'),
-  ('Keep building cool stuff!');
+-- insert into todos(title)
+-- values
+--   ('Create Supabase project'),
+--   ('Create Next.js app from Supabase Starter template'),
+--   ('Keep building cool stuff!');
