384ecef1592b752138bc65fc5eab656b7337854c
Kevin Xu <k3vn.xu@gmail.com>
2023-10-21 11:51:10 -0400

    raise user prop

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,18 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
 import LogoutButton from './LogoutButton'
 import LoginButton from './LoginButton'
 
-export default async function Header() {
-  const supabase = createServerComponentClient({ cookies })
 
-  const { data: { user } } = await supabase.auth.getUser()
-
+export default async function Header({ user }) {
   return (
     <nav className="w-full flex justify-center h-16">
-      <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
+      <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground border border-2" >
         {user ? (
           <div className="flex items-center gap-4">
             Hey, {user.email}!
             <LogoutButton />
           </div>
         ) : (
-          <LoginButton/>
+          <LoginButton />
         )}
       </div>
     </nav>
