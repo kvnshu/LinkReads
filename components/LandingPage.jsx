10c4446e673d7ccb5e9c22af9ca1c12aa1235391
Kevin Xu <k3vn.xu@gmail.com>
2023-10-31 13:02:04 -0400

    style landing page

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -3,11 +3,11 @@ import LoginButton from "./LoginButton";
 
 export default function LandingPage() {
   return (
-    <div id="hero">
-      <div id="hero-container" className="">
-        <div className="">
-          <h1 className="font-sans font-bold text-xl">Goodreads for internet articles</h1>
-          <h2 className="font-sans font-medium text-lg">Keep track of what you read online, and see what your friends are reading</h2>
+    <div id="hero" className="flex mt-48 items-center justify-center ">
+      <div id="hero-container">
+        <div>
+          <p className="text-6xl font-semibold">Goodreads for internet articles</p>
+          <p className="text-lg">Keep track of what you read online, and see what your friends are reading</p>
         </div>
         <LoginButton />
       </div>
