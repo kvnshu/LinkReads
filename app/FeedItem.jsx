3109d2cfd017e76caac7c393c09b037636c5f016
Kevin Xu <k3vn.xu@gmail.com>
2023-11-22 04:17:34 -0600

    huumanize timestamp

--- app/FeedItem.jsx
+++ app/FeedItem.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Link from 'next/link';
+import { formatDistanceToNow, parseISO } from "date-fns";
 
 export default function FeedItem({ data }) {
+  function parseAndHumanizeDate(dateString) {
+    const parsedDate = parseISO(dateString);
+    return formatDistanceToNow(parsedDate, { addSuffix: true });
+  }
+
   return (
     <div>
       <p>
         <Link href={`/user/${data.user_id}`}>Someone you follow</Link>{` finished reading ${data.links.url}`}
       </p>
-      <p>{`${data.read_at}`}</p>
+      <p>{`${parseAndHumanizeDate(data.read_at)}`}</p>
     </div>
   )
 }
