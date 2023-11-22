a0a4a84b6ab1404e6843920bf7f267c78f07d05f
Kevin Xu <k3vn.xu@gmail.com>
2023-11-22 01:42:39 -0600

    finish making root page rendered serverside

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,26 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 import Header from "@/components/Header"
 import LandingPage from "./LandingPage"
 import Dashboard from "./Dashboard"
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies })
-
-  const {
-    data: { session },
-  } = supabase.auth.getSession()
+  const { data: { session }, error } = await supabase.auth.getSession()
 
   return (
     <div className="container min-h-screen flex items-center flex-col">
       <Header
         user={session?.user}
       />
-      {session?.user ? (
-        <Dashboard session={session} />
-      ) : (
-        <LandingPage />
-      )
+      {
+        session ? (
+          <Dashboard session={session} />
+        ) : (
+          <div>
+            <LandingPage />
+          </div>
+        )
       }
     </div>
   )
