f3ba98b2f8d3868adee28d44a43dc7026fc033b7
Kevin Xu <k3vn.xu@gmail.com>
2023-11-22 10:56:10 -0600

    refactor LoginButton

--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -1,11 +1,26 @@
 'use client'
+import { useState } from 'react';
 import { Button } from "@nextui-org/button";
-import { Link } from "@nextui-org/link";
+import { useRouter } from 'next/navigation';
 
 export default function LoginButton() {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+
+  function handleClick() {
+    setIsLoading(true)
+    router.push('/login');
+    setIsLoading(false)
+  }
+
   return (
-    <div>
-      <Button href="/login" as={Link} color="primary" variant="flat">
+
+    <div className="self-center">
+      <Button 
+        onPress={handleClick}
+        color="primary"
+        variant="flat"
+        isLoading={isLoading}>
         Login
       </Button>
     </div>
