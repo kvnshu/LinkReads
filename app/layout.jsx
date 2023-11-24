6fdbc15a73663a1b61202327f136408c9ea6b2ef
Kevin Xu <k3vn.xu@gmail.com>
2023-11-24 10:57:08 -0600

    add google analytics to app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Providers } from "./providers";
+import Script from "next/script"
 
 export const metadata = {
   title: 'LinkReads',
@@ -11,6 +12,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light">
       <body>
+        <div className="analytics">
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-TPXV40DFYJ" />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-TPXV40DFYJ');
+            `}  
+          </Script>
+        </div>
         <Providers>
           {children}
         </Providers>
