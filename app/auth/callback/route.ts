c4a745ad345ed186c79945c01070599b90d4f035
Kevin Xu <k3vn.xu@gmail.com>
2023-12-30 09:32:44 -0600

    add full_name to profile

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,8 +8,19 @@ export async function GET(request: Request) {
   const next = searchParams.get('next') ?? '/'
   if (code) {
     const supabase = createSupabaseAppServerClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) {
+    const { error: authError } = await supabase.auth.exchangeCodeForSession(code)
+
+    // update profiles database 
+    const { data: { session }, error: sessionError } = await supabase.auth.getSession()
+    const user = session?.user
+
+    // TODO: to optimize, implement conditional logic to check if full_name is already added off of user.last_sign_in_at
+    const { error: profileError } = await supabase
+      .from('profiles')
+      .update({ full_name: user?.user_metadata?.full_name })
+      .eq('id', user?.id)
+
+    if (!authError && !profileError) {
       return NextResponse.redirect(`${origin}${next}`)
     }
   }
