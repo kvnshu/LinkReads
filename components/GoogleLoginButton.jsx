46f9de3675598e3ea7668f589d22849b14e01700
Kevin Xu <k3vn.xu@gmail.com>
2024-01-04 01:44:59 -0600

    dynamically select auth callback route based on environment

--- components/GoogleLoginButton.jsx
+++ components/GoogleLoginButton.jsx
@@ -4,11 +4,25 @@ import { createSupabaseFrontendClient } from "@/utils/supabaseBrowser"
 
 export default function GoogleLoginButton() {
   const supabase = createSupabaseFrontendClient()
+
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/'
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+    url = `${url}auth/callback`
+    return url
+  }
+
   async function handleGoogleLogin() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: process.env.NEXT_PUBLIC_OAUTH_CALLBACK_URL,
+        redirectTo: getURL(),
       },
     })
   }
